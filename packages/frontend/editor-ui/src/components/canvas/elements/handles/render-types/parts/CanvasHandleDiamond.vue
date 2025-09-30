16c508ec699a56357a34331dcae3c4991bf61c79
Tuukka Kantola <tuukka@n8n.io>
2025-09-30 13:40:55 +0200

    Don't color sub-node handles with status colors

--- packages/frontend/editor-ui/src/components/canvas/elements/handles/render-types/parts/CanvasHandleDiamond.vue
+++ packages/frontend/editor-ui/src/components/canvas/elements/handles/render-types/parts/CanvasHandleDiamond.vue
@@ -1,35 +1,16 @@
 <script lang="ts" setup>
-import { computed } from 'vue';
-
-const props = withDefaults(
+withDefaults(
 	defineProps<{
 		handleClasses?: string;
-		executionStatus?: string;
-		hasPinnedData?: boolean;
 	}>(),
 	{
 		handleClasses: undefined,
-		executionStatus: undefined,
-		hasPinnedData: false,
 	},
 );
-
-const statusClasses = computed(() => {
-	if (props.hasPinnedData) {
-		return 'pinned';
-	}
-	if (['error', 'crashed'].includes(props.executionStatus || '')) {
-		return 'error';
-	}
-	if (props.executionStatus === 'success') {
-		return 'success';
-	}
-	return 'default';
-});
 </script>
 
 <template>
-	<div :class="[$style.diamond, handleClasses, $style[statusClasses]]" />
+	<div :class="[$style.diamond, handleClasses]" />
 </template>
 
 <style lang="scss" module>
@@ -42,39 +23,7 @@ const statusClasses = computed(() => {
 	border: 2px solid var(--canvas--background);
 
 	&:hover {
-		background-color: var(--color-primary);
-	}
-
-	&.pinned {
-		background-color: var(--color-node-pinned-border);
-
-		&:hover {
-			background-color: var(--color-secondary-shade-1);
-		}
-	}
-
-	&.success {
-		background-color: var(--color-success);
-
-		&:hover {
-			background-color: var(--color-success-shade-1);
-		}
-	}
-
-	&.error {
-		background-color: var(--color-danger);
-
-		&:hover {
-			background-color: var(--color-danger-shade-1);
-		}
-	}
-
-	&.default {
-		background-color: var(--node-handle--background);
-
-		&:hover {
-			background-color: var(--color-primary);
-		}
+		background: var(--color-primary);
 	}
 }
 </style>
